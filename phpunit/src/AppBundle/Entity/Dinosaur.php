4c333ce6b5f24cb364c5ab5e68813263469f1d77
Ibrahima <icamara@wynd.eu>
2018-08-21 21:14:51 +0200

    using datafixture for functional tests

--- phpunit/src/AppBundle/Entity/Dinosaur.php
+++ phpunit/src/AppBundle/Entity/Dinosaur.php
@@ -81,6 +81,11 @@ class Dinosaur
         return $this->isCarnivorous;
     }
 
+    public function setEnclosure(Enclosure $enclosure)
+    {
+        $this->enclosure = $enclosure;
+    }
+
 
 
 }
